f5c1eb94c9f3bb5ee623829844a0efc386d9d6fc
Babybay <satriab2108@gmail.com>
2026-05-07 13:25:09 +0800

    feat(redesign): remove ClosingStatement section from homepage

--- apps/storefront/src/app/(store)/page.tsx
+++ apps/storefront/src/app/(store)/page.tsx
@@ -2,7 +2,6 @@ import { getHomepage } from '@/lib/sanity'
 import HeroSilent from '@/components/home/HeroSilent'
 import LatestDrop from '@/components/home/LatestDrop'
 import LookbookStrip from '@/components/home/LookbookStrip'
-import ClosingStatement from '@/components/home/ClosingStatement'
 
 export const revalidate = 0
 
@@ -19,7 +18,6 @@ export default async function HomePage() {
       <HeroSilent videoUrl={data?.heroVideo} imageUrl={data?.heroImage} />
       <LatestDrop collection={data?.featuredCollection ?? null} />
       <LookbookStrip items={(data?.lookbookImages ?? []).filter((i: any) => i?.image)} />
-      <ClosingStatement statement={data?.closingStatement || 'BUILT FOR THOSE WHO MOVE DIFFERENTLY'} />
     </>
   )
 }
